471ecb33780e20d7e0cad0b6cc3a4e9ebf2c8162
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-03-24 13:30:35 +0100

    add script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 /******************************************
- * SURNAME LIST
+ * LAST NAME LIST
  *****************************************/
+
+/*
+ TASKS
+ 1 - creazione arrey cognomi
+ 2 - chiedere cognome a utente
+ 3 - inserirlo in arrey cognomi
+ 4 - ordinare arrey cognomi in ordine alfabetico
+ 5 - stampare arrey cognomi
+ 6 - stampare posizione del cognome dell'utente con base 1
+*/
+
+//1
+var lastnameList = ["rossi", "bianchi", "verdi", "facchini", "colombo", "milesi"];
+
+//2
+var userLastname = prompt("What's your Surname?").toLowerCase().trim();
+
+//3
+lastnameList.push(userLastname);
+//console.log(lastnameList);
+
+//4
+lastnameList.sort();
+
+//5
+console.log(lastnameList);
+
+//6
+console.log( lastnameList.indexOf(userLastname) + 1 );
