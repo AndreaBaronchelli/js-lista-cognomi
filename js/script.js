68c96bb5f47b94726e26fe1884dc3a56a6e57f70
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-03-24 13:52:33 +0100

    innerhtml

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-/******************************************
+/*****************************************
  * LAST NAME LIST
- *****************************************/
+*****************************************/
 
 /*
  TASKS
@@ -10,6 +10,8 @@
  4 - ordinare arrey cognomi in ordine alfabetico
  5 - stampare arrey cognomi
  6 - stampare posizione del cognome dell'utente con base 1
+ 7 - stampare nell'html la lista
+ 8 - stampare nell'html la posizione 
 */
 
 //1
@@ -26,7 +28,18 @@ lastnameList.push(userLastname);
 lastnameList.sort();
 
 //5
-console.log(lastnameList);
+//console.log(lastnameList);
 
 //6
-console.log( lastnameList.indexOf(userLastname) + 1 );
+//console.log( lastnameList.indexOf(userLastname) + 1 );
+
+//7
+var list = "";
+for(var i = 0; i < lastnameList.length; i++) {
+    list += "<li>" + lastnameList[i] + "</li>";
+}
+
+document.getElementById("lastname-list").innerHTML = list;
+
+//8
+document.getElementById("position").innerHTML = "Your position is the number " + (lastnameList.indexOf(userLastname) + 1);
